18fd95eea22cc6a705e5c3ab4a4ce2dd367a22f2
yliu123 <yang.7.liu@nokia.com>
2019-01-20 21:39:11 +0100

    Initial Commit

---
+++ LotusBoucheCousue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LotusBoucheCousue
+//
+//  Created by Liu, Yang 7. (Nokia - FR/Paris-Saclay) on 20/01/2019.
+//  Copyright © 2019 LaurenceLiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LotusBoucheCousueTests/LotusBoucheCousueTests.swift
@@ -0,0 +1,34 @@
+//
+//  LotusBoucheCousueTests.swift
+//  LotusBoucheCousueTests
+//
+//  Created by Liu, Yang 7. (Nokia - FR/Paris-Saclay) on 20/01/2019.
+//  Copyright © 2019 LaurenceLiu. All rights reserved.
+//
+
+import XCTest
+@testable import LotusBoucheCousue
+
+class LotusBoucheCousueTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
